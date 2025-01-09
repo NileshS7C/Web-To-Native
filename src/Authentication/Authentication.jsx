cbd0064c811103bab8e06903ae3aa2324fefdbce
abhisheksingh7904 <abhishek.singh@seventhtriangle.com>
2025-01-09 14:43:07 +0530

    removed the link from index

--- src/Authentication/Authentication.jsx
+++ src/Authentication/Authentication.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
 const Authentication = ({ children }) => {
-  const [cookies, setCookie] = useCookies("refreshToken");
+  const [cookies] = useCookies("refreshToken");
   const isLoggedIn = cookies.refreshToken;
   if (isLoggedIn) {
     return children;
