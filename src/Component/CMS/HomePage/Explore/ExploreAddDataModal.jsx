ed6adf8549763e3d371add6843e411d8cebc3c24
abhisheksingh7904 <abhishek.singh@seventhtriangle.com>
2025-02-15 23:51:49 +0530

    scrolling added in the card modal in the cms

--- src/Component/CMS/HomePage/Explore/ExploreAddDataModal.jsx
+++ src/Component/CMS/HomePage/Explore/ExploreAddDataModal.jsx
@@ -27,7 +27,7 @@ export default function ExploreAddDataModal({ data, isOpen, onClose, fetchHomepa
             <DialogBackdrop className="fixed inset-0 bg-gray-500/75 transition-opacity" />
             <div className="fixed inset-0 z-10 w-screen">
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                    <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                    <DialogPanel className="relative max-h-[90vh] transform overflow-y-auto rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                         <Formik
                             initialValues={{
                                 title: "",
