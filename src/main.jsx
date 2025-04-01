95c6b2c05536547cd6b199bffe2123aef8f521fe
abhisheksingh7904 <abhishek.singh@seventhtriangle.com>
2025-04-01 17:13:55 +0530

    skeleton library styling import

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import "react-awesome-slider/dist/styles.css";
 import "react-quill/dist/quill.snow.css";
+import "react-loading-skeleton/dist/skeleton.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
