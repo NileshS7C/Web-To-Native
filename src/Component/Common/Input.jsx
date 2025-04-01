59910896508be5944e7b46f1ad075d6ba7a6a395
abhisheksingh7904 <abhishek.singh@seventhtriangle.com>
2025-04-01 17:13:02 +0530

    resuable input

---
+++ src/Component/Common/Input.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+
+export const Input = ({
+  className,
+  type,
+  id,
+  name,
+  ref,
+  onChange,
+  value,
+  disabled,
+  ...props
+}) => {
+  return (
+    <input
+      type={type}
+      id={id}
+      name={name}
+      className={` ${
+        type === "file"
+          ? "file:mr-4 file:py-2 file:px-4 file:rounded-[10px] file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          : "w-full px-[19px] border-[1px] border-[#DFEAF2] rounded-[15px] h-[50px] focus:outline-none focus:ring-2 focus:ring-blue-500  "
+      }`}
+      ref={ref}
+      onChange={onChange}
+      value={value}
+      disabled={disabled}
+    />
+  );
+};
+
+Input.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.name,
+  type: PropTypes.string,
+  ref: PropTypes.any,
+};
