7640d5a5d1b5035e9137711c3868a299f8ef700a
abhisheksingh7904 <abhishek.singh@seventhtriangle.com>
2025-04-01 17:14:58 +0530

    Image uploading hook

---
+++ src/Hooks/CMS/useImageUpload.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { uploadImage } from "../../redux/Upload/uploadActions";
+
+import { venueImageSize } from "../../Constant/app";
+
+export const useImageUpload = () => {
+  const dispatch = useDispatch();
+  const [isError, setIsError] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadError, setUploadError] = useState("");
+  const [previewURL, setPreviewURL] = useState(null);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+
+  const handleFileUpload = async (e) => {
+    console.log(" i am working inside this function");
+    setIsError(false);
+    setUploadError("");
+    const uploadedFile = e.target.files[0];
+
+    if (!uploadedFile.type.startsWith("image/")) {
+      setUploadError("File should be a valid image type.");
+      return;
+    }
+
+    const maxSize = venueImageSize;
+    if (uploadedFile.size > maxSize) {
+      setUploadError("File should be less than 5 MB");
+      return;
+    }
+    try {
+      setIsUploading(true);
+      setUploadSuccess(false);
+      const result = await dispatch(uploadImage(uploadedFile)).unwrap();
+      const url = result.data.url;
+      setPreviewURL(url);
+      setUploadSuccess(true);
+    } catch (err) {
+      setUploadError(err.data?.message);
+      setIsError(true);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  return {
+    handleFileUpload,
+    isError,
+    isUploading,
+    uploadError,
+    previewURL,
+    uploadSuccess,
+  };
+};
