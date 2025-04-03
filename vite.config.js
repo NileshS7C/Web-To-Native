e45e0b31d4101415a74f6ef187d7e02f152efab8
Anchal617 <95863209+Anchal617@users.noreply.github.com>
2025-04-03 17:49:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: true,
+    strictPort: true,
+  },
+  preview: {
+    allowedHosts: ["admin.uat.picklebay.com"], // Explicitly allow the domain
+  },
   resolve: {
     extensions: [".js", ".jsx", ".json"],
   },
@@ -13,9 +21,5 @@ export default defineConfig({
 
   optimizeDeps: {
     include: ["react-quill"],
-  },
-
-  preview: {
-    allowedHosts: ["admin.uat.picklebay.com"],
   },
 });
