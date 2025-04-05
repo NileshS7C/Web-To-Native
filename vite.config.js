c80913b62015891cb1feeeefd0df399f0e7d18e1
PranavAggarwalSTC <pranav.aggarwal@seventhtriangle.com>
2025-04-05 22:50:09 +0530

    vite added

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    host: true,
+    strictPort: true,
+  },
+  preview: {
+    allowedHosts: ["admin.uat.picklebay.com"], // Explicitly allow the domain
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase size limit to 1000kb
+  },
+
+  optimizeDeps: {
+    include: ["react-quill"],
+  },
+});
