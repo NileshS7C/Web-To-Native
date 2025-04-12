3116d930591c11dcca17158d16b9ffe2994a5eaa
Anchal617 <Anchal@seventhtriangle.com>
2025-04-12 23:18:19 +0530

    Update EventCreationModal.jsx

--- src/Component/Tournament/Event/EventCreationModal.jsx
+++ src/Component/Tournament/Event/EventCreationModal.jsx
@@ -305,7 +305,7 @@ export const EventCreationModal = () => {
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in  w-full max-w-xs sm:max-w-md lg:max-w-[70%]  sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in  w-full max-w-xs sm:max-w-md lg:max-w-[70%] max-h-[90vh]  sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 overflow-y-auto"
           >
             <div>
               <div className="w-full bg-[#FFFFFF] px-[20px] h-full overflow-y-hidden">
