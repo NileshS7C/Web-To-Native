5c62805f3fce3481f7a4c212532ad5eab911ac25
HarshVardhanS7C <harshvardhan@seventhtriangle.com>
2025-04-25 17:20:47 +0530

    Image-Edit-Placeholder

--- src/Component/Common/LocationSearch.jsx
+++ src/Component/Common/LocationSearch.jsx
@@ -95,7 +95,7 @@ const LocationSearchInput = ({ id, name, setFieldValue,isEdit }) => {
           className="w-full px-[19px] border-[1px] border-[#DFEAF2] rounded-[15px] h-[50px] focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="text"
           value={query}
-          placeholder={`Search... ${location?.name || ""}`}
+          placeholder="Search..."
           onChange={(e) => {
             handleInputChange(e);
           }}
