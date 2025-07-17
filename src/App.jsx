a315aaad560dab3d03c997202f5ee57d5679bff2
Nilesh Pushkarna <nilesh.pushkarna@seventhtriangle.com>
2025-07-17 16:11:37 +0530

    Web to native

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import AllRoutes from "./Routes/AllRoutes";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setMobileConfig } from "./redux/WebToNative/webToNativeSlice";
 
 function App() {
+  // const platform = useSelector((state) => state.wtn.platform);
+  const APP_SECRET = import.meta.env.VITE_APP_SECRET;
+  console.log("app secret", APP_SECRET)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    window.handleMobileConfig = (secret = "", platform = "browser") => {
+      if (secret !== APP_SECRET) {
+        alert('Unauthorized config attempt!');
+        return false;
+      }
+      const validPlatforms = ['android', 'ios'];
+      if (
+        validPlatforms.includes((platform || '').toLowerCase())
+      ) {
+        dispatch(setMobileConfig({
+          platform: platform.toLowerCase(),
+        }));
+        return true;
+      } else {
+        alert('Invalid config: platform must be "android" or "ios" and isMobileApp must be a boolean.');
+        return false;
+      }
+    };
+    return () => {
+      delete window.handleMobileConfig;
+      console.log("Script loaded")
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   if (platform === 'ios' || platform === 'android') {
+  //     console.log("Script loaded successfully")
+  //   }
+  // }, [platform]);
+
   return (
     <div className="App">
-       <Toaster position="bottom-center" />
+      <Toaster position="bottom-center" />
       <AllRoutes />
     </div>
   );
---
+++ src/redux/WebToNative/webToNativeSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const initialState = {
+    platform: 'Browser',
+};
+
+const wtnSlice = createSlice({
+    name: 'wtn',
+    initialState,
+    reducers: {
+        setMobileConfig(state, action) {
+            state.platform = action.payload.platform;
+        },
+    },
+});
+
+export const wtnStore = configureStore({
+    reducer: {
+        wtn: wtnSlice.reducer,
+    },
+});
+
+export const { setMobileConfig } = wtnSlice.actions;
+
+
+export default wtnSlice.reducer; 
