c6e18e7bb5ffebcd21ee717c360d461e80922e62
Nilesh Pushkarna <nilesh.pushkarna@seventhtriangle.com>
2025-07-21 16:40:46 +0530

    ..

--- src/redux/tournament/tournamentActions.js
+++ src/redux/tournament/tournamentActions.js
@@ -766,31 +766,32 @@ export const downloadSheetOfPlayers = createAsyncThunk(
           fileName = decodeURIComponent(fileName);
         }
       }
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
 
-      alert("Url", url)
-      console.log("url,.,,,,.>>>,", url)
       if (platform === "android") {
-        const mimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-        const blob = new Blob([response.data], { type: mimeType });
-        const blobUrl = window.URL.createObjectURL(blob);
-        window.WTN.customFileDownload({
-          fileName: fileName,
-          downloadUrl: blobUrl,
-          mimeType: mimeType,
-          cookies: "",
-          isBlob: true,
-          userAgent: "",
-          openFileAfterDownload: true
-        })
+        const reader = new FileReader();
+        reader.readAsDataURL(response.data);
+        reader.onloadend = () => {
+          const base64data = reader.result.split(',')[1];
+          window.WTN.customFileDownload({
+            fileName: fileName,
+            downloadUrl: base64data,
+            mimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            cookies: "",
+            isBlob: false,
+            userAgent: "",
+            openFileAfterDownload: true
+          });
+        };
+      } else {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+        window.URL.revokeObjectURL(url);
       }
-      link.setAttribute("download", fileName);
-      document.body.appendChild(link);
-      link.click();
-      link.remove();
-      window.URL.revokeObjectURL(url);
     } catch (err) {
       if (
         err.response &&
